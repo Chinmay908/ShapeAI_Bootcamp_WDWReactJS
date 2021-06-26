61c8c3a6c07681ceb8848f34172d539206424861
Chinmay908 <69110832+Chinmay908@users.noreply.github.com>
2021-06-26 17:33:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Info from "./Info";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic Web Developement React JS Bootcamp. This bootcamp was taken by Shaurya Sinha. We covered everything from scratch including HTML, CSS, Javascript and Reat JS</p>
+    </div>
+  );
+}
+export default Info;
